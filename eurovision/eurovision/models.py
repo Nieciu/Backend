7f91ccd785e35ffcc7b8df8314371fa4412f778e
Nieciu <nieciowy@gmail.com>
2024-03-14 22:06:01 +0100

    added models.py; added serializers.py added eurovison to apps

---
+++ eurovision/eurovision/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Country(models.Model):
+    code = models.CharField(max_length=2, primary_key=True)
+    country_name = models.Charfield(max_length=100)
+
+    def __str__ (self):
+        return self.country_name
+    
+    class Meta:
+        verbose_name_plural = "Countries"
