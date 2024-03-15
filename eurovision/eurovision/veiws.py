46dd176e4ea463d88e6ed87d49f19a1e7f6d9c5e
Nieciu <nieciowy@gmail.com>
2024-03-15 11:53:49 +0100

    added views.py that sends country list

---
+++ eurovision/eurovision/veiws.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+from .models import Country
+from .serializers import CountrySerializer
+
+def country_list(request):
+
+    countries = Country.objects.all()
+    serializer = CountrySerializer(countries, many=True)
+    return JsonResponse(serializer.data)
