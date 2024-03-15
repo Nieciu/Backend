fdc34b1d5581431e4d0d8b7bba8b7d393bfcc134
Nieciu <nieciowy@gmail.com>
2024-03-15 21:54:54 +0100

    I'm losing my religion

--- eurovision/eurovision/settings.py
+++ eurovision/eurovision/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-os4cdux=^3l6udm)oltz@oh!ez=s=*97ah%$pc+xzf=v5o-q15'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
