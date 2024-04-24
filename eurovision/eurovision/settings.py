a249cf8d9938280e8cfb48a2019fbd98ecd81691
Nieciu <nieciowy@gmail.com>
2024-04-24 18:57:28 +0200

    Added support for postgres

--- eurovision/eurovision/settings.py
+++ eurovision/eurovision/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = 'eurovision.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': os.environ.get("DB_NAME"),
+
+        'USER': os.environ.get("DB_MASTER_USERNAME"),
+
+        'PASSWORD': os.environ.get("DB_MASTER_PASSWORD"),
+
+        'HOST': os.environ.get("DB_ENDPOINT"),
+
+        'PORT': os.environ.get("DB_PORT"),
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
