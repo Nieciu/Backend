7f512866a5e357a9c932a92ddf31c605e165738f
Nieciu <nieciowy@gmail.com>
2024-04-28 23:30:37 +0200

    Add VoterAdminForm to customize admin interface in Eurovision project. Also added some new models to be registered

--- eurovision/eurovision/admin.py
+++ eurovision/eurovision/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-from .models import Country
+from .models import Country, Voter, Contestant, Song
 
-admin.site.register(Country)
+admin.site.register(Country, Contestant, Song)
+
+class VoterAdminForm(admin.ModelAdmin):
+    model = Voter
+    fields = ['first_name', 'last_name']
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        obj.save()
+
+admin.site.register(Voter, VoterAdminForm)
