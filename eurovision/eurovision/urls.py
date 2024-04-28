c61b21c11f29eed59ca22912317114446022c894
Nieciu <nieciowy@gmail.com>
2024-04-29 01:40:37 +0200

    Add new API endpoints for voters and votes in Eurovision project

--- eurovision/eurovision/urls.py
+++ eurovision/eurovision/urls.py
@@ -4,6 +4,19 @@ from eurovision import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    
     path('countries/', views.country_list),
-    path('api-auth/', include('rest_framework.urls'))
+    # path('countries/<str:country_name>/', views.country_detail),
+    # path('contestants/', views.contestant_list),
+    # path('contestants/<str:artist>/', views.contestant_detail),
+    # path('songs/', views.song_list),
+    # path('songs/<str:title>/', views.song_detail),
+    # all votes of the user
+    path('votes/<str:username>/', views.vote_list),
+    # here you can PUT a vote
+    # everything else is just a GET
+    path('votes/<str:username>/<str:song>/', views.vote_detail),
+    # take username from here
+    path('voters/', views.voter_list),
 ]
