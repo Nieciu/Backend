c77970b9def23313e19083bf2463e2cead860de0
Nieciu <nieciowy@gmail.com>
2024-04-28 23:16:14 +0200

    Update models and add signal to create votes for new voters in Eurovision project

--- eurovision/eurovision/models.py
+++ eurovision/eurovision/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Country(models.Model):
     country_name = models.CharField(max_length=100, primary_key=True)
-    euro_flag_url = models.URLField(max_length=300, null=True, default=None) #TBD later
-    final_order = models.IntegerField(default=None, null=True)
+    euro_flag_url = models.URLField(max_length=300, null=True, default=None, blank=True) #TBD later
+    final_order = models.IntegerField(default=None, null=True, blank=True)
 
     def __str__ (self):
         return self.country_name
@@ -14,7 +16,7 @@ class Country(models.Model):
 class Contestant(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     artist = models.CharField(max_length=100, primary_key=True)
-    about = models.TextField(default=None, null=True)
+    about = models.TextField(default=None, null=True, blank=True)
 
     def __str__ (self):
         return self.artist
@@ -25,9 +27,9 @@ class Contestant(models.Model):
 class Song(models.Model):
     contestant = models.ForeignKey(Contestant, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, primary_key=True)
-    yt_video_url = models.URLField(max_length=200, default=None, null=True)
-    lyrics_original = models.TextField(default=None, null=True)
-    lyrics_translated = models.TextField(default=None, null=True)
+    yt_video_url = models.URLField(max_length=200, default=None, null=True, blank=True)
+    lyrics_original = models.TextField(default=None, null=True, blank=True)
+    lyrics_translated = models.TextField(default=None, null=True, blank=True)
 
     def __str__ (self):
         return self.title
@@ -55,7 +57,7 @@ class Vote(models.Model):
     voter = models.ForeignKey(Voter, on_delete=models.CASCADE)
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
     #1. Song quality - lyrics, melody, and harmony
-    song_quality = models.IntegerField(default=None,null=True)
+    song_quality = models.IntegerField(default=None, null=True)
     #2. Stage presence - costume, lighting, and choreography
     stage_presence = models.IntegerField(default=None, null=True)
     #3. Vocal performance - pitch, tone, and range
@@ -66,3 +68,9 @@ class Vote(models.Model):
     
     class Meta:
         verbose_name_plural = "Votes"
+
+@receiver(post_save, sender=Voter)
+def create_votes(sender, instance, created, **kwargs):
+    if created:
+        for song in Song.objects.all():
+            Vote.objects.create(voter=instance, song=song)
