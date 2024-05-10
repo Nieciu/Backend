0df5b23de17318c2696657b7c5b501bd28bf456c
Nieciu <nieciowy@gmail.com>
2024-05-10 14:58:30 +0200

    Update country_list view to exclude songs without final order and order by final order

--- eurovision/eurovision/views.py
+++ eurovision/eurovision/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def country_list(request):
-    songs = Song.objects.all()
+    songs = Song.objects.exclude(contestant__country__final_order__isnull=True).order_by('contestant__country__final_order')
     data = []
     for song in songs:
         data.append({
