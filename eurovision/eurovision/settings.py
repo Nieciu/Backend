cf3780b2d29a6e99862239edd92432c0b2c131a0
Nieciu <nieciowy@gmail.com>
2024-05-10 17:35:10 +0200

    Updated STATIC_ROOT in settings.py to use environment variable

--- eurovision/eurovision/settings.py
+++ eurovision/eurovision/settings.py
@@ -131,11 +131,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.environ.get("STATIC")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 # CORS
 
